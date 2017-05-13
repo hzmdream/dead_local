57113b4d697d4bd06d7b6d32073ee8a1bf5083fd
houzm <houzm@usky.com.cn>
2017-05-13 13:10:53 +0800

    后台管理系统组件创建

---
+++ dl-bg-html/src/main/java/org/dl/bg/html/App.java
@@ -0,0 +1,13 @@
+package org.dl.bg.html;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
